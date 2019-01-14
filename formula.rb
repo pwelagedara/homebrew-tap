8f4ac1c0411a88b3ef65c8fa82b369ad6de3b7f5
pwelagedara <pubuduwelagedara@gmail.com>
2019-01-14 13:56:13 -0500

    initial commit

---
+++ formula.rb
@@ -0,0 +1,10 @@
+class Formula < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/pwelagedara/formula/archive/v1.0.0.tar.gz"
+  sha256 "89788638f467efb859062e8b2eba579afbfb748eaf0d10e1fddebb619a681481"
+
+  def install
+    bin.install "formula"
+  end
+end
