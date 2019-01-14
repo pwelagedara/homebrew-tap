2b26f41e524773207ad4249f7ff7c03c2f0fc208
pwelagedara <pubuduwelagedara@gmail.com>
2019-01-14 14:17:02 -0500

    second commit

---
+++ hello.rb
@@ -0,0 +1,10 @@
+class Hello < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/pwelagedara/hello/archive/v1.0.0.tar.gz"
+  sha256 "3b8c437f71f6c9a9ce8002683a96783296e743c019b2a882b0e62ddd102f6ffb"
+
+  def install
+    bin.install "formula"
+  end
+end
